01eb0d0b806dfc8b1f1c061d0f476df5840b917e
st3inum <st3inum@gmail.com>
2019-02-11 14:36:42 +0600

    Add files via upload

---
+++ github.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<iterator>
+#include <iomanip>
+
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// b8keLaSGs4FmPth
+// bit.ly/2DVzRNS
+
+int main(int argc, char const *argv[])
+{
+	printf("THANK YOU BOLOD\n");
+	return 0;
+}
